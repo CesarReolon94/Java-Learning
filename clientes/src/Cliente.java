945732b0d63b16bce727b780a1ee67d6ad0fafc3
unknown <cesar.reolon94@gmail.com>
2021-02-15 18:22:44 -0300

    organização em pasta clientes

---
+++ clientes/src/Cliente.java
@@ -0,0 +1,25 @@
+
+public class Cliente {
+	//a classe se limita a ter atributos privados com getters e setters, sendo considerada uma classe fantoche ou Value Objects
+	private String nome;
+	private String cpf;
+	private String profissao;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+}
