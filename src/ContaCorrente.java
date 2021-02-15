8d39064c3e3c3415810c062d8a83cdebb2121b0a
unknown <cesar.reolon94@gmail.com>
2021-02-15 18:11:22 -0300

    commitando sistema de contas

---
+++ src/ContaCorrente.java
@@ -0,0 +1,26 @@
+//herança não vale para construtores
+public class ContaCorrente extends Conta implements Tributavel {
+
+	
+	public ContaCorrente(int agencia, int numero) {
+		//chamando construtor na super class
+		super(agencia, numero);
+	}
+	
+	@Override
+	public boolean saca(double valor) {
+		double valorASacar = valor + 0.2;
+		return super.saca(valorASacar);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		super.saldo += valor;		
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo * 0.01;
+	}
+	
+}
